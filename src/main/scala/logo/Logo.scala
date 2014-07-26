e22facf574563868dedfc6cdfba4de6f41ef9bb3
Guy John <guy@rumblesan.com>
2014-07-26 11:21:35 +0100

    Cleaning up main code

--- src/main/scala/logo/Logo.scala
+++ src/main/scala/logo/Logo.scala
@@ -9,27 +9,32 @@ import com.rumblesan.logo.turtle.Turtle.double2Float
 
 class Logo extends PApplet {
 
-  var ls: LSystem = _
+  val system = HilbertCurve
+
+  // Needs to be a var because it gets changed on each
+  // mouse click
+  var ls: LSystem = LSystem(system.seed, system.rules)
 
   val lineSize = 5.0
 
-  val system = HilbertCurve
+  val turtle = Turtle(
+    width * 0.05,
+    height * 0.85,
+    system.startAngle,
+    Nil,
+    this
+  )
 
   override def setup {
     size(1024, 768)
     smooth()
     frameRate(30)
-
-    ls = new LSystem(system.seed, system.rules)
-
   }
 
   override def draw {
 
     background(0)
     stroke(255)
-
-    val turtle = Turtle(width * 0.05, height * 0.85, system.startAngle, Nil, this)
 
     system(lineSize).parse(ls, turtle)
 
